edc6a026218c1498383e356f9e3d34007b61309b
Ryan Panela <52223024+ryanapanela@users.noreply.github.com>
2025-07-22 06:17:26 +0000

    updated __init__.py to include segmentation and recall functions

--- event_recall_module/__init__.py
+++ event_recall_module/__init__.py
@@ -2,5 +2,5 @@
 
 __version__ = '0.1.0'
 
-from .segmentation import run_segmentation
-from .recall import run_recall_scoring
+from .segmentation import segmentation, run_segmentation
+from .recall import evaluate_recall, recall_matrix, recall_score
