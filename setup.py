b06f9dec56a77d5ed86dbbc7b8a8beef2ccbbdc6
Ryan Panela <52223024+ryanapanela@users.noreply.github.com>
2025-10-15 11:13:04 -0400

    Updated setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,8 @@ setup(
     version="0.1.0",
     description="Event Segmentation Applications for LLM-Enabled Automated Recall Scoring",
     author="Ryan A. Panela",
+    py_modules=["segmentation", "recall", "cli", "utils"],
+    package_dir={"": "module"},
     packages=find_packages(),
     install_requires=[
         "openai",
